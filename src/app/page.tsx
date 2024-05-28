7f56c88dea989a0047b2c9da0209f688930e3d82
Ankit <rattanankit2004@gmail.com>
2024-05-28 22:22:34 +0530

    added analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Contact from "./contact/page";
 import Enter from "./enter/page";
 import Main from "./main/page";
 import Navbar from "./navbar/page";
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function Home() {
   return (
     <>
+    <Analytics/>
     <Navbar/>
     <Main/>
     <About/>
